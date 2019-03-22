711df2acbdb763808ec74f27f5d2ec7bb9b2925e
Ashley Smith <ashleysmith@zipcoders-MacBook-Pro.local>
2019-03-22 10:27:42 -0400

    first commit all test are passing

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,10 @@
+import com.sun.org.apache.xerces.internal.impl.xpath.regex.Match;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -9,31 +13,76 @@ public class HamletParser {
 
     private String hamletData;
 
-    public HamletParser(){
+    public HamletParser() {
         this.hamletData = loadFile();
     }
 
-    private String loadFile(){
+    private String loadFile() {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("hamlet.txt").getFile());
         StringBuilder result = new StringBuilder("");
 
-        try(Scanner scanner = new Scanner(file)){
-            while(scanner.hasNextLine()){
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
             }
 
             scanner.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return result.toString();
     }
 
-    public String getHamletData(){
+    public String getHamletData() {
         return hamletData;
     }
 
+    public void changeHamletToLeon() {
+       Pattern pattern = Pattern.compile("\\bHamlet|Hamlets\\b");
+       Matcher matcher = pattern.matcher(hamletData);
+        hamletData =  matcher.replaceAll("Leon");
+    }
+
+    public void changeHoratioToTariq() {
+        Pattern pattern = Pattern.compile("\\bHoratio|Horatios\\b");
+        Matcher matcher = pattern.matcher(hamletData);
+        hamletData = matcher.replaceAll("Tariq");
+    }
+
+    public String changeName(String nameToFind, String nameToReplaceWith) {
+        Pattern pattern = Pattern.compile("\\w+(\\W)?");
+
+        Matcher matcher = pattern.matcher(nameToFind);
+
+        String result = "";
+
+        while (matcher.find()) {
+            String name = matcher.group();
+            if (name.equals(nameToFind)) {
+                result += nameToReplaceWith;
+            } else {
+                result += name;
+            }
+        }
+
+
+        return result;
+
+    }
+
+    public Boolean findHoratio(String hamletData) {
+        Pattern pattern1 = Pattern.compile("Hamlet | HAMLET");
+//        Pattern pattern2 = Pattern.compile("HAMLET");
+        return (pattern1.matcher(hamletData).find());
+//
+    }
+
+    public boolean findHamlet(String hamletData) {
+        Pattern pattern1 = Pattern.compile("Hamlet | HAMLET");
+//        Pattern pattern2 = Pattern.compile("HAMLET");
+        return (pattern1.matcher(hamletData).find());
+    }
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,28 +1,43 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class HamletParserTest {
-    private String hamletText;
-    private HamletParser hamletParser;
-
-    @Before
-    public void setUp() {
-        this.hamletParser = new HamletParser();
-        this.hamletText = hamletParser.getHamletData();
-    }
-
     @Test
     public void testChangeHamletToLeon() {
+        // given
+        HamletParser hamletParser = new HamletParser();
+        String hamletText = hamletParser.getHamletData();
+        String expectedText = hamletText.replaceAll("Hamlet", "Leon");
+
+        // when
+        hamletParser.changeHamletToLeon();
+
+        // then
+        String actualText = hamletParser.getHamletData();
+        Assert.assertEquals(expectedText, actualText);
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        // given
+        HamletParser hamletParser = new HamletParser();
+        String hamletText = hamletParser.getHamletData();
+        String expectedText = hamletText.replaceAll("Horatio", "Tariq");
+
+        // when
+        hamletParser.changeHoratioToTariq();
+
+        // then
+        String actualText = hamletParser.getHamletData();
+        Assert.assertEquals(expectedText, actualText);
     }
 
     @Test
     public void testFindHoratio() {
+
     }
 
     @Test
